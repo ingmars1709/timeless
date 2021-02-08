2539b24f8fc91f34e99b9ebbe414d14c917ae74b
Ingmar <ingmars@infosupport.com>
2021-02-08 18:21:46 +0100

    Comment

--- src/main/java/com/stonesoft/timeless/controller/TimelessController.java
+++ src/main/java/com/stonesoft/timeless/controller/TimelessController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TimelessController {
+
+    // Comment
+
     @GetMapping
     public String test() {
         return "successful";
