fbe214f2317c3224aff422811e6b9d877dc640a2
Ingmar <ingmars@infosupport.com>
2021-02-28 18:10:16 +0100

    Endpoint to exploit side channel attack

--- src/main/java/com/stonesoft/timeless/controller/TimelessController.java
+++ src/main/java/com/stonesoft/timeless/controller/TimelessController.java
@@ -1,15 +1,20 @@
 package com.stonesoft.timeless.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.MessageDigest;
 
 @RestController
 public class TimelessController {
 
-    // Comment3
+    private static final String secretCode = "9456794567945679456794567945679456794567";
 
-    @GetMapping
-    public String test() {
-        return "successful";
+    @GetMapping("/try")
+    public String test(@RequestParam String code) {
+        //final boolean unsecureIsEqual = secretCode.equals(code);
+        final boolean equal = MessageDigest.isEqual(secretCode.getBytes(), code.getBytes());
+        return code;
     }
 }
