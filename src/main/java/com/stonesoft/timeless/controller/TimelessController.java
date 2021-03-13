e83a1de8ff7282ad1376f70f363baca7da3c620a
Ingmar <ingmars@infosupport.com>
2021-03-13 18:35:20 +0100

    Student answers running example

--- src/main/java/com/stonesoft/timeless/controller/TimelessController.java
+++ src/main/java/com/stonesoft/timeless/controller/TimelessController.java
@@ -4,22 +4,56 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.security.MessageDigest;
+import java.util.List;
+
+import static java.util.Arrays.asList;
 
 @RestController
 public class TimelessController {
 
-    private static final String secretCode = "9456794567945679456794567945679456794567";
+    private static final List<String> correctAnswers = asList("a","c","b","c");
 
-    @GetMapping("/try")
-    public boolean test(@RequestParam String code) {
-        final boolean unsecureIsEqual = secretCode.equals(code);
-        return unsecureIsEqual;
+    @GetMapping("/unsafeCheck")
+    public boolean unsafeCheck(
+                        final @RequestParam String answer1,
+                        final @RequestParam String answer2,
+                        final @RequestParam String answer3,
+                        final @RequestParam String answer4) {
+
+        return unsafeCheckCorrectAnswers(asList(answer1, answer2, answer3, answer4));
     }
 
-    @GetMapping("/trysafe")
-    public boolean testSafe(@RequestParam String code) {
-        final boolean secureEqual = MessageDigest.isEqual(secretCode.getBytes(), code.getBytes());
-        return secureEqual;
+    @GetMapping("/safeCheck")
+    public boolean safeCheck(
+                        final @RequestParam String answer1,
+                        final @RequestParam String answer2,
+                        final @RequestParam String answer3,
+                        final @RequestParam String answer4) {
+
+        return safeCheckCorrectAnswers(asList(answer1, answer2, answer3, answer4));
     }
+
+    private boolean safeCheckCorrectAnswers(final List<String> givenAnswers) {
+        // TODO
+        return true;
+    }
+
+    private boolean unsafeCheckCorrectAnswers(final List<String> givenAnswers) {
+        for (int i = 0; i < givenAnswers.size(); i++) {
+            if (!correctAnswer(givenAnswers.get(i), i)) {
+             return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean correctAnswer(final String studentAnswer, final int questionNumber) {
+        try {
+            Thread.sleep(200); // do some processing
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return correctAnswers.get(questionNumber).equals(studentAnswer);
+    }
+
 }
